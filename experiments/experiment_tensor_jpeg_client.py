566a77c91ea6992ad1fc0052f5fdfb060b38efdc
rex.yu <jiafish@outlook.com>
2024-06-17 12:53:06 +0000

    functional jpeg

---
+++ experiments/experiment_tensor_jpeg_client.py
@@ -0,0 +1,18 @@
+################################### setting path ###################################
+import sys
+sys.path.append('../')
+################################### import libs ###################################
+import cv2
+from  pytorchyolo import detect, models_split_tiny
+from pytorchyolo.utils.transforms import Resize, DEFAULT_TRANSFORMS
+import torchvision.transforms as transforms
+import numpy as np
+from pytorchyolo.utils.utils import load_classes, rescale_boxes, non_max_suppression, print_environment_info
+import pandas as pd
+import time
+import torch
+import torchvision.ops.boxes as bops
+import matplotlib.pyplot as plt
+from torchvision import io as tvio
+
+################################### Varialbe init ###################################
