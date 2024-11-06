5cb44e8d9149213283402e2ae428a5bbcb3a7e96
Rexyyj <jiafish@outlook.com>
2024-11-06 11:27:10 +0100

    add measurements

--- stable_tests/experiment_script/run.sh
+++ stable_tests/experiment_script/run.sh
@@ -1,9 +1,9 @@
 # python3 ./experiment_client_sketchml.py 8
 # sleep 3
-python3 ./experiment_client_jpeg.py 8
+python3 ./experiment_client_jpeg_per_frame.py 8
 sleep 3
-python3 ./experiment_client_regression.py 8
+python3 ./experiment_client_regression_per_frame.py 8
 sleep 3
-python3 ./experiment_client_decomposition.py 8
+python3 ./experiment_client_decomposition_per_frame.py 8
 
 
